0abf2aca2f7cdec795f17b5facd10f32c9525794
musekorea <musekorea@hanmail.net>
2022-04-04 21:50:02 +0800

    argument to graphql

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 import { createServer } from "@graphql-yoga/node";
 
+const familyData = [
+	{ id: 1, name: "moya", age: 4 },
+	{ id: 2, name: "baozi", age: 5 },
+	{ id: 3, name: "nicai", age: 8 },
+];
+
+const typeDefs = `
+	type Family {
+		id : Int!
+		name : String!
+		age : Int!
+	}
+	type Query {
+		family : [Family]
+		member(id:Int) : Family
+
+	}	
+`;
+
 const server = createServer({
 	schema: {
-		typeDefs,
-		resolvers,
+		typeDefs: typeDefs,
+		resolvers: {
+			Query: {
+				family: () => familyData,
+				member: (_, args) => {
+					const filtered = familyData.filter((member) => {
+						return args.id === member.id;
+					});
+					return filtered[0];
+				},
+			},
+		},
 	},
 });
 
