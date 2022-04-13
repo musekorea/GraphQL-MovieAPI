b02a73d0a1ce2b14140c818ea0bc2ee4d9d20f56
musekorea <musekorea@hanmail.net>
2022-04-13 20:43:05 +0800

    fix query

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 import { createServer } from "@graphql-yoga/node";
 import fetch from "node-fetch";
 
-const API_URL = `https://yts.torrentbay.to/api/v2/list_movies.json`;
+const API_URL = "https://yts.torrentbay.to/api/v2/list_movies.json";
 
-const getMovieData = async (limit, rating) => {
-	const url = `${API_URL}?${limit ? "limit=" + limit : ""}&${
-		rating ? "minimum_rating=" + rating : ""
-	}`;
-	const fetchDatas = await fetch(url);
-	const MovieDatas = await fetchDatas.json();
-	return MovieDatas.data.movies;
+const getMovieData = async (limit, rating, sort_by) => {
+	const temp_sort = "peers";
+	const url = `${API_URL}?${limit ? "limit=" + limit : ""}${
+		rating ? "&minimum_rating=" + rating : ""
+	}${sort_by ? "&sort_by=" + sort_by : ""}&order_by=desc`;
+	try {
+		const fetchDatas = await fetch(url);
+		const movieDatas = await fetchDatas.json();
+		const movieLists = movieDatas.data.movies;
+		console.log(url);
+		console.log(movieLists[0].year);
+		return movieLists;
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const getMovieDetail = async (id) => {
 	const url = `https://yts.torrentbay.to/api/v2/movie_details.json?movie_id=${id}`;
 	const fetchDetail = await fetch(url);
 	const detailData = await fetchDetail.json();
-	console.log(detailData);
 	return detailData.data.movie;
 };
 
@@ -34,10 +41,14 @@ const typeDefs = `
 		rating : Float!
 		description_full : String!
 		language : String!
-		medium_cover_image : String
+		medium_cover_image : String!
+		background_image : String!
+		year : Int!
+
+
 	}
 	type Query {
-		movies(limit:Int, rating:Float) : [Movie]!
+		movies(limit:Int, rating:Float, sort_by:String) : [Movie]!
 		movie(id:Int!) : Movie!
 		suggestion(id:Int!):[Movie]
 	}
@@ -48,7 +59,8 @@ const server = createServer({
 		typeDefs: typeDefs,
 		resolvers: {
 			Query: {
-				movies: (_, { limit, rating }) => getMovieData(limit, rating),
+				movies: (_, { limit, rating, sort_by }) =>
+					getMovieData(limit, rating, sort_by),
 				movie: (_, { id }) => getMovieDetail(id),
 				suggestion: (_, { id }) => getMovieSuggestion(id),
 			},
